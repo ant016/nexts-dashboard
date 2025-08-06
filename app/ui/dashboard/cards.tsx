40a6495f1cde6ef40bb904f6ce9c606c9bcce413
ant016_ <a.valdes.carretero03@gmail.com>
2025-08-06 16:50:53 +0200

    Fetching de datos con streaming HTML en componentes del Dashboard

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -14,18 +14,20 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  // TODO: Replace these placeholder values with real data fetching logic
+
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/*<Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />*/}
     </>
   );
 }
